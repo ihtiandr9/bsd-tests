f08a433c876e623ffba1046b697716bae13cff6c
Sergey Ryabchenkov(fvds) <lomaster@ihtiandr9.fvds.ru>
2024-02-02 19:19:57 +0300

    daemon checked in bsd

---
+++ daemon/solution.c
@@ -0,0 +1,32 @@
+/*
+   Program fork self and forked part close all links
+   with terminal and sleep.
+ */
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <dlfcn.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
+{
+    int pid = fork();
+    if (pid)
+    {
+        printf("%d\n", pid);
+    }
+    else
+    {
+        setsid();
+        fclose(stdout);
+        fclose(stdin);
+        fclose(stderr);
+        while (1)
+            sleep(20);
+    }
+
+    return 0;
+}
+
