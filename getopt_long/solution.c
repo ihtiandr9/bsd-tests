e84e305d3bfb031943c7c65841f3c163b8e1e30f
Sergey Ryabchenkov(fvds) <lomaster@ihtiandr9.fvds.ru>
2025-05-29 11:02:00 +0300

    getopt_long

---
+++ getopt_long/solution.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+
+struct option opts[] =
+{
+	{"query", required_argument, 0, 'q'},
+	{"longinformationrequest", no_argument, 0, 'i'},
+	{"version", no_argument, 0, 'v'},
+	{0, 0, 0, 0}
+};
+
+int main(int argc, char *argv[])
+{
+    int query = 0;
+    int argerr = 0;
+    int rez;
+    int long_index;
+
+    while( -1 != (rez = getopt_long(argc, argv, "q:iv", opts, &long_index)))
+    {
+        switch (rez)
+        {
+            case 'i':
+                printf("found argument \"i\".\n");
+                break;
+            case 'q':
+                printf("found argument \"q\".\n");
+                query = atoi(optarg);
+                if (!query) argerr = 1;
+                break;
+            case 'v':
+                printf("found argument \"v\".\n");
+                break;
+            default:
+                argerr = 1;
+        }
+    }
+
+    if(argerr) printf("-"); else printf("+");
+    return 0;
+}
